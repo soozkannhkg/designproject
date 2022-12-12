f05352f9215b9fbce7646e21b66f60cd2da6c499
kaangokcedag <hkgokcedag@gmail.com>
2022-12-12 19:59:23 +0300

    3

--- app/src/main/java/com/hkg/designproject/AnaMenu.kt
+++ app/src/main/java/com/hkg/designproject/AnaMenu.kt
@@ -26,7 +26,7 @@ class AnaMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setTitle("GUVENLINSAAT")
-        //selamýn alikim
+        //selamýn alikim222222
         setContentView(R.layout.activity_ana_menu)
         val yenidokumanlink = findViewById(R.id.textView6) as TextView
         yenidokumanlink.setOnClickListener{
