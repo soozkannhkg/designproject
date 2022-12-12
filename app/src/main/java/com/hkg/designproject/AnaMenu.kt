6cef0f51285d111baf04460d1bcf31e8cf6014f6
kaangokcedag <hkgokcedag@gmail.com>
2022-12-12 20:05:38 +0300

    d12.12 h20.25 haci

--- app/src/main/java/com/hkg/designproject/AnaMenu.kt
+++ app/src/main/java/com/hkg/designproject/AnaMenu.kt
@@ -26,7 +26,7 @@ class AnaMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setTitle("GUVENLINSAAT")
-        //selamýn alikim2222223333344444555666667888
+        //selamýn alikim22222233333444445556666678889
         setContentView(R.layout.activity_ana_menu)
         val yenidokumanlink = findViewById(R.id.textView6) as TextView
         yenidokumanlink.setOnClickListener{
